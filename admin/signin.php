97b1e375ea59c8c6c301b73c33de0e8f9f83decb
dizzyeyes <909338300@qq.com>
2015-05-25 03:39:10 +0800

    beautified sign in

--- admin/signin.php
+++ admin/signin.php
@@ -19,6 +19,11 @@ if(!isset($_SESSION['user_id'])){
             $data = mysqli_query($dbc,$query);
             if(mysqli_num_rows($data)==1){
                 $error_msg = "Sorry, username:'$user_username' has been taken.Please input another one.";
+                echo "
+                    <div class='alert alert-block alert-danger' onclick='this.style.display=\"none\";'>
+                    <strong>$error_msg</strong>
+                    </div>
+                    ";
             }
             else
             {
@@ -31,6 +36,11 @@ if(!isset($_SESSION['user_id'])){
             }
         }else{
             $error_msg = 'Sorry, you must enter a valid username and password to sign in.';
+            echo "
+                <div class='alert alert-block alert-danger' onclick='this.style.display=\"none\";'>
+                <strong>$error_msg</strong>
+                </div>
+                ";
         }
     }
 }else{//如果用户已经登录，则直接跳转到已经登录页面
@@ -41,51 +51,71 @@ if(!isset($_SESSION['user_id'])){
 <html>
     <head>
         <title>实验室预约系统 - 注册</title>
-        <link rel="stylesheet" type="text/css" href="style.css" />
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
-        <style>
-            body {
-                font-family: Monospace;
-                background-color: #f0f0f0;
-                margin: 0px;
-                overflow: hidden;
-            }
-            .acc{
-                z-index:10;
-            } 
-        </style>
+        
+        <link href="css/prettify.css" rel="stylesheet" />
+        <link href="css/bootstrap.2.1.0.css" rel="stylesheet">
+        <link href="css/demosignin.css" rel="stylesheet" />
+        <script Charset="UTF-8" src="libs/jquery-2.1.3.js" type="text/javascript"></script>
+        <script Charset="UTF-8" src="libs/prettify.js" type="text/javascript"></script>
+        <script Charset="UTF-8" src="js/dialogProcess.js" type="text/javascript"></script>
+        <script Charset="UTF-8" src="js/fadeInFadeOut.js" type="text/javascript"></script>
     </head>
     
     <body>
-        <h3 style="top:20%;left:45%;position:absolute;">实验室预约系统 - 注册</h3>
         <!--通过$_SESSION['user_id']进行判断，如果用户未登录，则显示登录表单，让用户输入用户名和密码-->
         <?php
         if(!isset($_SESSION['user_id'])){
-            echo '<p class="error">'.$error_msg.'</p>';
+        //    echo '<p class="error">'.$error_msg.'</p>';
         ?>
         <!-- $_SERVER['PHP_SELF']代表用户提交表单时，调用自身php文件 -->
-        <div style="position:absolute;top:30%;left:45%;">
+        <div style="position:absolute;top:10%;left:35%;">
             <form method = "post" action="<?php echo $_SERVER['PHP_SELF'];?>"　>
-                <div  style="text-align: center;">
+                <div  class="well span6">
                     <fieldset>
-                        <legend>注　册</legend>
-
-                        <label for="username">用户名:</label>
-                        <!-- 如果用户已输过用户名，则回显用户名 -->
-                        <input type="text" id="username" name="username"
-                        value="<?php if(!empty($user_username)) echo $user_username; ?>" />
-
-                        <br/>
-
-                        <label for="password">密　码:</label>
-                        <input type="password" id="password" name="password"/>
-
+                        <div style="text-align: center;">
+                            <legend>
+                                <h3 >实验室预约系统<br>
+                                注　　册</h3>
+                            </legend>
+                        </div>
+                        <div >
+                            <div>
+                                <label for="username">用户名:</label>
+                                <!-- 如果用户已输过用户名，则回显用户名 -->
+                                <input class="span6" type="text" id="username" name="username"
+                                value="<?php if(!empty($user_username)) echo $user_username; ?>" />
+                            </div>
+                            <div>
+                                <label for="password">密　码:</label>
+                                <input class="span6" type="password" id="password" name="password"/>
+                            </div>
+                            <div>
+                                <label for="user_type">用户类型:</label>
+                                <input class="span6" type="text" id="user_type" name="user_type"/>
+                            </div>
+                            <div>
+                                <label for="email">邮　箱:</label>
+                                <input class="span6" type="email" id="email" name="email"/>
+                            </div>
+                            <div>
+                                <label for="tel">手机号:</label>
+                                <input class="span6" type="tel" id="tel" name="tel"/>
+                            </div>
+                            <div>
+                                <label for="info">备注信息:</label>
+                                <input class="span6" type="text" id="info" name="info"/>
+                            </div>
+                            <div style="text-align: right;">
+                                <input type="submit" class="btn btn-info" value="注　册" name="submit"/>
+                            </div>
+                        </div>
                     </fieldset>
-                    <input type="submit" value="注　册" name="submit"/>
                 </div>
             </form>
-        </div>
+        </div>        
+        
         <?php
         }
         ?>
